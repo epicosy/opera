fd86b2a3c98364218b947a5b50e8be1182e8d1d3
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-01-20 00:23:16 -0800

    fix implicit type error

--- context/repositories.tsx
+++ context/repositories.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, {createContext, useContext, Dispatch, SetStateAction, useState, FC, ReactNode} from "react";
-import {RepositoriesPagination} from "../typings";
+import {RepositoriesPagination, Repository} from "../typings";
 import {ApolloClient, gql, InMemoryCache, useQuery} from "@apollo/client";
 import DropdownWithCheckboxes from "../components/Dropdowns/CheckboxDropdown";
 import Link from "next/link";
@@ -44,8 +44,8 @@ interface RepositoriesPageContextProps {
     selectedLanguage: string[];
     setSelectedLanguage: Dispatch<SetStateAction<string[]>>;
     pagination: RepositoriesPagination;
-    headers: string[];
-    rows: React.FunctionComponent[];
+    headers: (string | React.JSX.Element)[];
+    rows: any[];
 }
 
 const RepositoriesPageContext = createContext<RepositoriesPageContextProps>({
@@ -55,15 +55,24 @@ const RepositoriesPageContext = createContext<RepositoriesPageContextProps>({
     setSelectedAvailability: () => {},
     selectedLanguage: [],
     setSelectedLanguage: () => {},
-    pagination: {},
+    pagination: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        totalPages: 0,
+        totalResults: 0,
+        page: 0,
+        perPage: 10,
+        pages: [],
+        elements: []
+    },
     headers: [],
     rows: []
 } as RepositoriesPageContextProps);
 
 export const RepositoriesPageProvider: FC<{children: ReactNode}> = ({children}) => {
     const [currentPage, setPage] = useState(1);
-    const [selectedAvailability, setSelectedAvailability] = React.useState([]);
-    const [selectedLanguage, setSelectedLanguage] = React.useState([]);
+    const [selectedAvailability, setSelectedAvailability] = React.useState<boolean[]>([]);
+    const [selectedLanguage, setSelectedLanguage] = React.useState<string[]>([]);
     const repositoriesPageQuery = useQuery(LIST_REPOSITORIES,
         {client, variables: {page: currentPage, per_page: PAGE_SIZE, availability: selectedAvailability,
                 language: selectedLanguage}}
@@ -85,14 +94,15 @@ export const RepositoriesPageProvider: FC<{children: ReactNode}> = ({children}) 
 
     const headers = ["Id", "Owner", "Name", availabilityDrop, languagesDrop, "Topics", "Commits Count"];
 
-    const rows : React.FunctionComponent[] = pagination.elements.map((repo: any) => {
+    const rows = pagination.elements.map((repo: any) => {
         return [<Link href={`http://localhost:3005/repositories/${repo.id}/`} target="_blank"
                       className="text-blue-600 dark:text-blue-500 hover:underline" >{repo.id}</Link>,
             repo.owner, repo.name, repo.available, repo.language, repo.topics.join(", "), repo.commitsCount]
     });
 
     return (
-        <RepositoriesPageContext.Provider value={{currentPage, setPage, headers, rows, pagination}}>
+        <RepositoriesPageContext.Provider value={{currentPage, setPage, selectedAvailability, setSelectedAvailability,
+            selectedLanguage, setSelectedLanguage, pagination, headers, rows}}>
             {children}
         </RepositoriesPageContext.Provider>
     )
